b33894b118918f95fad29e92be508c8f7f93710f
Marcos Reyes <astronautmarkus@gmail.com>
2026-02-14 23:39:48 -0300

    fix: se corrige el typo del nombre del seeder de Web Plans en ingles.

--- reyesandfriends-web-app/database/seeders/DatabaseSeeder.php
+++ reyesandfriends-web-app/database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             ServiceSeeder::class,
-            WebPlanesSeeder::class,
+            WebPlansSeeder::class,
         ]);
     }
 }
